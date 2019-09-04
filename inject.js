ce9cd1734c52c90bb3c5ff380e3eb7b7e684c7fd
Suqian Zhang <zhang@oculavis.de>
2019-09-05 01:12:48 +0200

    Added basic encrypted msg valication

--- inject.js
+++ inject.js
@@ -1,26 +1,51 @@
 // const notes = document.getElementsByClassName('IZ65Hb-TBnied');
+
 // Get all the notes
 const notes = document.querySelectorAll('[aria-multiline="true"]');
 const noteClasses = notes[1].classList; // note[1] is always the input note bar
+// Get the uglified class name of the note's content
 const noteContentClass = noteClasses[noteClasses.length - 1];
+
+// let note, noteContent;
 
 Array.from(notes).forEach(function (el) {
     el.style.backgroundColor = "gray";
     el.addEventListener("click", _ => {
-        checkEditableNote(el, this);
+        getEditableNote(el, this);
     });
 });
 
-checkEditableNote = function(div, callback) {
+getEditableNote = function(div, callback) {
     setTimeout(function(){
         const openedNote = document.getElementsByClassName(noteContentClass);
         Array.from(openedNote)
             .filter(el => el.innerHTML != '')
             .filter(el => el.getAttribute('contenteditable') == "true")
             .forEach(el => {
-                alert(el.innerHTML);
-            })
+                checkNote(el.parentElement, el.innerHTML.replace(/<br>/g, ""));
+            });
     }
-    ,2000); 
+    ,2000);
     div.removeEventListener("click",callback);
+}
+
+isValidEncryptJson = function (note) {
+    try{
+        const encrypJson = JSON.parse(note);
+        if (encrypJson.hasOwnProperty("iv")) {
+            return true;
+        }
+        return false;
+    }
+    catch(e) {
+        return false;
+    }
+}
+
+checkNote = function (note, noteCOntent) {
+    
+    if (!isValidEncryptJson(noteCOntent))
+        return;
+    
+    alert("An encrypted note found!!!\n\n" + noteCOntent);
 }
