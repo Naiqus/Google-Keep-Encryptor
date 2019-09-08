0a960fa9949c2687c4e3b407b66e12c1edd60e81
Suqian Zhang <zhang@oculavis.de>
2019-09-09 00:54:42 +0200

    Improved lock button state control. Added more button states

--- injection/inject.js
+++ injection/inject.js
@@ -15,12 +15,12 @@ const btnsOverlay = document.createElement("div");
 btnsOverlay.classList.add("btn-overlay");
 
 const lockBtn = document.createElement("div");
-lockBtn.name = "decrypt_btn";
-lockBtn.addEventListener("click", event => {
-    event.stopPropagation();
-    //alert(decryptNote(encryptedText, "iqui9oob"));
-    showPasswordInput();
-});
+lockBtn.name = "crypt_btn";
+lockBtn.classList.add("cryptor-btn");
+btnsOverlay.appendChild(lockBtn);
+
+const lockBtnCallbacks = [showPasswordCallBack, encryptNoteCallback, decryptNoteCallback, createEncryptedNoteCallback];
+
 const lockedHtml = `<svg focusable="false" data-icon="lock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path class="icon" fill="white" d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"></path></svg>`
 const unlockedHtml = `<svg focusable="false" data-icon="unlock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path class="icon" fill="currentColor" d="M400 256H152V152.9c0-39.6 31.7-72.5 71.3-72.9 40-.4 72.7 32.1 72.7 72v16c0 13.3 10.7 24 24 24h32c13.3 0 24-10.7 24-24v-16C376 68 307.5-.3 223.5 0 139.5.3 72 69.5 72 153.5V256H48c-26.5 0-48 21.5-48 48v160c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path></svg>`;
 
@@ -40,6 +40,7 @@ noteOverlay.style.borderColor = color;
 
 let cipherText, plainText, openedNote, isNoteEncrypted;
 let pinButtonClasses;
+let isDecryptSuccess = false;
 
 showBtnsOverlay(notes[1]);
 
@@ -59,13 +60,13 @@ function verifyEncryptJson(note) {
     }
 }
 
-function findPopupNote() {
+function getOpenedNote() {
     const editableNotes = document.getElementsByClassName(noteContentClass);
     Array.from(editableNotes)
         .filter(el => el.innerHTML != '')
         .filter(el => el.getAttribute('contenteditable') == "true")
         .forEach(el => {
-            handlePopupNote(el);
+            handleOpenedNote(el);
             return;
         });
     // No popup note found.
@@ -76,7 +77,7 @@ function findPopupNote() {
     }
 }
 
-function handlePopupNote(el) {
+function handleOpenedNote(el) {
     openedNote = el;
     let text = el.innerHTML.replace(/<br>/g, "");
 
@@ -84,31 +85,19 @@ function handlePopupNote(el) {
 
     if (verifyEncryptJson(text)) {
         showLockIcon();
+        setLockBtnCallback(showPasswordCallBack);
         cipherText = text;
+        isNoteEncrypted = true;
     } else {
         showUnlockIcon();
+        setLockBtnCallback(createEncryptedNoteCallback);
         plainText = el.innerHTML;
+        isNoteEncrypted = false;
     }
-}
-
-function showPasswordInput() {
-    pwInput.innerText = "";
-    if (!btnsOverlay.contains(pwInput)){
-        btnsOverlay.insertBefore(pwInput, lockBtn);
-        pwInput.addEventListener("keydown", event => {
-            event.stopPropagation();
-            password = pwInput.value;
-            if (event.key === "Enter") {
-                let text = decryptNote(cipherText, password);
-                showNoteOverlay(text);
-                //pwInput.value = "";
-            }
-        });
-    }
-    pwInput.focus();
 }
 
 function showNoteOverlay(text) {
+    // Decrypt succeed
     openedNote.classList.forEach(cls => {
         noteOverlay.classList.add(cls);
     });
@@ -121,32 +110,25 @@ function showNoteOverlay(text) {
     noteOverlay.addEventListener("keydown", fixCallback);
     noteOverlay.addEventListener("keypress", fixCallback);
     noteOverlay.addEventListener("click", fixCallback);
-    noteOverlay.addEventListener("input", onNoteChange);
-
+    noteOverlay.addEventListener("input", event => {
+        event.stopPropagation();
+        lockBtn.click();
+    });
 }
 
 function showBtnsOverlay(parentElement){
     // Add button overlay
-    lockBtn.classList.add("decryptBtn");
-    // decryptButton.style.add("color: ")
-    lockBtn.classList.add("cryptorBtn");
-    btnsOverlay.appendChild(lockBtn);
     parentElement.parentElement.appendChild(btnsOverlay);
 
 }
 
-function onNoteChange(event) {
-    event.stopPropagation();
-    openedNote.innerHTML = encryptNote(pwInput.value, noteOverlay.innerHTML);
-    openedNote.dispatchEvent(new Event("input"));
-}
-
 function decryptNote(text, password) {
     try {
-        return sjcl.json.decrypt(password, text)
+        decryptText = sjcl.json.decrypt(password, text)
+        isDecryptSuccess = true;
+        return decryptText;
     } catch (e) {
         console.log(e);
-        //return e.msg;
     }
 }
 
@@ -157,7 +139,6 @@ function encryptNote(password, text) {
         return test;
     } catch (e) {
         console.log(e);
-        //return e.msg;
     }
 }
 
@@ -172,6 +153,59 @@ function showUnlockIcon(){
     const lockedIcon = lockBtn.getElementsByClassName("icon")[0];
     lockedIcon.style.fill = color;
 }
+
+//------------------
+// Lock button callbacks
+function showPasswordCallBack(event) {
+    console.log("showPasswordCallBack");
+    event.stopPropagation();
+    pwInput.innerText = "";
+    if (!btnsOverlay.contains(pwInput)){
+        btnsOverlay.insertBefore(pwInput, lockBtn);
+        pwInput.addEventListener("keydown", event => {
+            event.stopPropagation();
+            if (event.key === "Enter")
+                lockBtn.click();
+        });
+        // Now lock button will decrypt the note
+        setLockBtnCallback(decryptNoteCallback);
+    }
+    pwInput.focus();
+}
+
+function createEncryptedNoteCallback(event) {
+    console.log("createEncryptedNoteCallback");
+    event.stopPropagation();
+}
+
+function decryptNoteCallback(event) {
+    console.log("decryptNoteCallback");
+    event.stopPropagation();
+    password = pwInput.value;
+    let text = decryptNote(cipherText, password);
+    if (isDecryptSuccess) {
+        showUnlockIcon();
+        showNoteOverlay(text);
+        setLockBtnCallback(encryptNoteCallback);
+    }
+}
+
+function encryptNoteCallback(event) {
+    console.log("encryptNoteCallback");
+    event.stopPropagation();
+    if (isNoteEncrypted) {
+        openedNote.innerHTML = encryptNote(pwInput.value, noteOverlay.innerHTML);
+        openedNote.dispatchEvent(new Event("input")); 
+    }
+}
+
+function setLockBtnCallback(callback){
+    lockBtnCallbacks.forEach(cb => {
+        lockBtn.removeEventListener("click", cb);
+    });
+    lockBtn.addEventListener("click", callback);
+}
+
 //-----------
 // Monitoring
 // Callback function to execute when mutations are observed
@@ -186,7 +220,7 @@ const callback = function (mutationsList, observer) {
         setTimeout(_ => {
             if (mutationsList[0].type === 'childList') {
                 console.log('child node changed');
-                findPopupNote();
+                getOpenedNote();
                 observer.observe(document, config);
             }
         }, throttleDuration)
